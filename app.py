472a50e704b5d2c0c6bdadcc52dc2bedfd220a6b
SUHAIB  T <tvpsuhaib2@gmail.com>
2025-05-21 22:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     if st.button("Predict"):
         img_array = np.array(image)
