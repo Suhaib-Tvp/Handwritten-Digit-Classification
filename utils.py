aae0431c2cf5adebd9b7d55a5081c7623d267ffd
SUHAIB  T <tvpsuhaib2@gmail.com>
2025-05-24 19:07:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,18 @@
-# utils.py
-
 import numpy as np
 from PIL import Image, ImageOps
+import joblib
+
+def load_model():
+    return joblib.load("mnist_logreg.pkl")
 
 def preprocess_image(img):
     img = img.convert('L').resize((28, 28), Image.Resampling.LANCZOS)
     img = ImageOps.invert(img)
     img = np.array(img)
-    img = (img > 127).astype(np.float32)
+    img = (img > 127).astype(np.float32)  # Binarize
     img = img.reshape(1, -1)
     return img
 
 def predict_digit(model, processed_img):
     pred = model.predict(processed_img)[0]
-    return pred
+    return int(pred)
