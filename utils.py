0a3b49d2ca5ff0e8ee5cd1f70f0fed9e0274df75
SUHAIB  T <tvpsuhaib2@gmail.com>
2025-05-24 20:09:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,12 @@
+# utils.py
 import cv2
 import numpy as np
 
-def preprocess_image(image_file):
-    """Preprocess uploaded image file for MNIST model prediction."""
-    # Read image as grayscale
-    file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
-    img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
-    # Resize to 28x28
+def preprocess_image(image):
+    img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
     img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
-    # Invert colors (MNIST: white digit on black background)
     img = 255 - img
-    # Threshold to clean image
     _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
-    # Normalize
     img = img / 255.0
-    # Flatten
     img = img.reshape(1, -1).astype(np.float32)
     return img
